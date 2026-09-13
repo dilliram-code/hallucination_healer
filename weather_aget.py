45725d9ce1b8ed25b69ebf8c0b45cf2628ba9050
dilliram-code <chaudharydr23@gmail.com>
2026-09-13 16:50:55 +0545

    Initialize weather agent script with environment variable loading and error handling

--- weather_aget.py
+++ weather_aget.py
@@ -1,1 +1,19 @@
 import os 
+import requests
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_core.tools import tool 
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# load env varialbes
+GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
+WEATHER_API_KEY=os.getenv("WEATHER_API_KEY")
+
+if not GEMINI_API_KEY:
+  raise ValueError("GEMINI_API_KEY is missing from .env")
+
+if not WEATHER_API_KEY:
+  raise ValueError("WEATHER_API_KEY is missing from .env")
+
+# create the weather tool
